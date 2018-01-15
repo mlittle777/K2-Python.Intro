00c62f3bf2758cd47451f1f9543d41b0cfcdf22f
Mark Little <mlittle777@gmail.com>
2018-01-15 14:16:50 -0500

    add ipython-config to remote repo

---
+++ Linux_PC_Setup/ipython_config.py
@@ -0,0 +1,11 @@
+# place this file here:
+# ~/.ipython/profile_default
+
+c = get_config()
+
+# Run all nodes interactively
+c.InteractiveShell.ast_node_interactivity = "all"
+
+# within a single session:
+# from IPython.core.interactiveshell import InteractiveShell
+# InteractiveShell.ast_node_interactivity = "all"
